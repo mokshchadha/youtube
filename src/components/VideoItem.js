7594ab8aab1c662f9a200498745037df20ff2aa7
moksh <moksh@source.one>
2020-10-18 03:46:02 +0530

    styling added

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,17 @@
+import "./VideoItems.css";
 import React from "react";
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      <img alt="" src={video.snippet.thumbnails.medium.url} />
-      <div>{video.snippet.title}</div>
+    <div className="video-item item">
+      <img
+        className="ui image"
+        alt=""
+        src={video.snippet.thumbnails.medium.url}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -5,7 +5,7 @@ const VideoList = ({ videos }) => {
   const videoItems = videos.map((v) => {
     return <VideoItem video={v} />;
   });
-  return <div>{videoItems}</div>;
+  return <div className="ui relaxed divided list">{videoItems}</div>;
 };
 
 export default VideoList;
