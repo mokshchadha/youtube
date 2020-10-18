24441cbd87e047b223cc23494d72f8c2c013dced
moksh <moksh@source.one>
2020-10-18 16:57:24 +0530

    added the iframe embedded youtbe vids

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-  if (video)
+  if (video) {
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
     return (
       <div>
+        <div className="ui embed">
+          <iframe title="" src={videoSrc} />
+        </div>
         <div className="ui segment">
           <h4 className="ui header">{video.snippet.title}</h4>
           <p>{video.snippet.description}</p>
         </div>
       </div>
     );
+  }
   return <div></div>;
 };
 
